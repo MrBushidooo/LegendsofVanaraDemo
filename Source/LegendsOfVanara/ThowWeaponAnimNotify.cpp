62a5cc1ee152806dc52fdedaeacf195e1cedbdfd
Leonard Panging <leonardpanging@gmail.com>
2026-03-16 10:13:12 +0530

    Update ThowWeaponAnimNotify.cpp

--- Source/LegendsOfVanara/ThowWeaponAnimNotify.cpp
+++ Source/LegendsOfVanara/ThowWeaponAnimNotify.cpp
@@ -14,13 +14,10 @@ void UThowWeaponAnimNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequen
 	ALegendsOfVanaraCharacter* Character = Cast<ALegendsOfVanaraCharacter>(MeshComp->GetOwner());
 
 	if (Character && Character->GadaWeapon) {
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Yellow, TEXT("Throw Weapon Anim Notify Triggered"));
 
 		Character->GadaWeapon->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
 
 		FVector ThrowDirection = Character->GetControlRotation().Vector();
-		
-		GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Green, FString::Printf(TEXT("Throw Direction: %s"), *ThrowDirection.ToString()));
 
 		Character->GadaWeapon->ThrowWeapon(ThrowDirection);
 	}
